6cdace60b4f6a06e0e6b3acae65fad913c9e5a21
Aline Sanches <sanches.aline6@gmail.com>
2024-09-18 04:44:37 -0300

    Adiciona componente de produto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { ProductComponent } from './product/product.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
