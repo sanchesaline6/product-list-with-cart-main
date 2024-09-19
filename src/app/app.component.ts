ac1ba663f80952c14a11d6cebf31f98eb5a50646
Aline Sanches <sanches.aline6@gmail.com>
2024-09-19 08:23:17 -0300

    Adiciona componente de carrinho á página principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { ProductComponent } from './product/product.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CartComponent } from "./cart/cart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent],
+  imports: [RouterOutlet, ProductComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
