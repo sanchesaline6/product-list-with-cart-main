e14a80642c1c43bbc420d9a1e7cb549e1758bd96
Aline Sanches <sanches.aline6@gmail.com>
2024-09-21 09:10:26 -0300

    Adiciona injeção da classe de serviços dos produtos e ajusta layout para mostrá-los

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { ProductComponent } from './product/product.component';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CartComponent } from "./cart/cart.component";
+import { ProductService } from './product.service';
+import { Product } from './product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent, CartComponent],
+  imports: [RouterOutlet, ProductComponent, CartComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'product-list-with-cart-main';
+  productService: ProductService = inject(ProductService);
+  productList: Product[] =[];
+
+  constructor(){
+    this.productService.getAllProducts().then((productList: Product[]) => {
+      this.productList = productList;
+    });
+  }
 }
