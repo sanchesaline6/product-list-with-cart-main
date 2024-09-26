2ff27383acc52f8c7f6cdea098391ae1923ba6d9
Aline Sanches <sanches.aline6@gmail.com>
2024-09-26 05:50:09 -0300

    Adiciona funcionalidade do botão de inserção e remoção de itens do carrinho

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,13 +1,37 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+
   @Input() product!: Product;
+  quantity: number = 0;
+  isAddedToCart: boolean = false;
+  isButtonDisabled: boolean = false;
+
+  incrementQuantity() {
+    if(this.quantity < 11){
+      this.quantity++;
+    }
+  }
+  decrementQuantity() {
+    if(this.quantity > 1) {
+      this.quantity--;
+    }
+    else{
+      this.isAddedToCart = false;
+    }
+  }
+
+  addToCart() {
+    this.isAddedToCart = true;
+    this.quantity = 1;
+  }
 }
