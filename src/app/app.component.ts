6ccda6d6b3e4444e1ab2cb8178d0679bc9c2d9ce
Aline Sanches <sanches.aline6@gmail.com>
2024-10-06 09:56:01 -0300

    Inclui cart-service nos imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { ProductService } from './product.service';
 import { Product } from './product';
 import { CommonModule } from '@angular/common';
 import { CartItemComponent } from './cart-item/cart-item.component';
+import { CartService } from './cart.service';
+import { CartItem } from './cart-item';
 
 @Component({
   selector: 'app-root',
@@ -18,10 +20,14 @@ export class AppComponent {
   title = 'product-list-with-cart-main';
   productService: ProductService = inject(ProductService);
   productList: Product[] =[];
+  cartService: CartService = inject(CartService);
+  cart: CartItem[] = [];
 
   constructor(){
     this.productService.getAllProducts().then((productList: Product[]) => {
       this.productList = productList;
     });
+
+    this.cart = this.cartService.getCart();
   }
 }
